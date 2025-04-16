bdf019c67e8b9d7bb4b040ef7c844bc868270831
Alisa Seifer <alisa.flaxman@gmail.com>
2025-04-16 13:42:23 +0300

    add env file

--- mongoose.js
+++ mongoose.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose')
-const URL = 'mongodb+srv://user:123@cluster1.yb0cf.mongodb.net/sample_flax'
+require('dotenv').config(); // מייבא את המשתנים מה-.env
 
-const connect = async() =>{
+const URL = process.env.MONGO_URI;
+
+const mongoose = require('mongoose')
+
+const connect = async () => {
     await mongoose.connect(URL)
     console.log('connected to DB')
 }
 
 const userSchema = mongoose.Schema({
-    "id":String,
-    "name":String,
-    "email":String,
-    "phone":String
+    "id": String,
+    "name": String,
+    "email": String,
+    "phone": String
 })
-const userModel = mongoose.model("user",userSchema)
+const userModel = mongoose.model("user", userSchema)
 
 module.exports = {
     connect,
